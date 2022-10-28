5b17cbd96e162449893ed4cb4d0d8fe6eb8a80ee
Christopher Ng <chrng8@gmail.com>
2022-10-28 19:18:27 +0000

    Add TODO to drop route for NC26
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -2,6 +2,7 @@
 
 return [
 	'ocs' => [
+		// TODO remove this route for NC26
 		['name' => 'Api#getRelatedResources', 'url' => '/related/{providerId}/{itemId}', 'verb' => 'GET'],
 		['name' => 'Api#getRelatedAlternate', 'url' => '/related/{providerId}', 'verb' => 'GET']
 	],
