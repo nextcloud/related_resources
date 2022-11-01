b61c99f7d9ea015f6cca1eff736155b18a4668b2
blizzz <blizzz@arthur-schiwon.de>
2022-11-01 20:12:23 +0100

    Revert "[stable25] alternate route for complex itemid"

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -2,9 +2,7 @@
 
 return [
 	'ocs' => [
-		// TODO remove this route for NC26
-		['name' => 'Api#getRelatedResources', 'url' => '/related/{providerId}/{itemId}', 'verb' => 'GET'],
-		['name' => 'Api#getRelatedAlternate', 'url' => '/related/{providerId}', 'verb' => 'GET']
+		['name' => 'Api#getRelatedResources', 'url' => '/related/{providerId}/{itemId}', 'verb' => 'GET']
 	],
 	'routes' => []
 ];
