02bb6125c1f6ff7731920cc38f46f73ff51c9576
Christopher Ng <chrng8@gmail.com>
2022-11-01 18:16:55 +0000

    Remove completed remove route TODO - Done in https://github.com/nextcloud/related_resources/pull/126
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -2,7 +2,6 @@
 
 return [
 	'ocs' => [
-		// TODO remove this route for NC26
 		['name' => 'Api#getRelatedResources', 'url' => '/related/{providerId}/{itemId}', 'verb' => 'GET'],
 		['name' => 'Api#getRelatedAlternate', 'url' => '/related/{providerId}', 'verb' => 'GET']
 	],
