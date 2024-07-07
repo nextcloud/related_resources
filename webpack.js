6369a5f8aab7da7be4e23ebea3ba94cc5e191cf6
Andy Scherzinger <info@andy-scherzinger.de>
2024-07-07 17:22:25 +0200

    chore: Add WebpackSPDXPlugin to extract license information of built assets
    Co-authored-by: Ferdinand Thiessen <opensource@fthiessen.de>
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- webpack.js
+++ webpack.js
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+const WebpackSPDXPlugin = require('./build/WebpackSPDXPlugin.js')
 const path = require('path')
 
 webpackConfig.entry = {
@@ -22,5 +23,18 @@ webpackConfig.output = {
 	path: path.resolve('./js'),
 	filename: 'related_resources.js',
 }
+
+webpackConfig.plugins = [
+	...webpackConfig.plugins,
+	// Generate reuse license files
+	new WebpackSPDXPlugin({
+		override: {
+			// TODO: Remove if they fixed the license in the package.json
+			'@nextcloud/axios': 'GPL-3.0-or-later',
+			'@nextcloud/vue': 'AGPL-3.0-or-later',
+			'nextcloud-vue-collections': 'AGPL-3.0-or-later',
+		}
+	}),
+]
 
 module.exports = webpackConfig
