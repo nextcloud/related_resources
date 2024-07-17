4406e43040823afca59b96c68c1f9a66591b855b
Jamie McClelland <jm@mayfirst.org>
2024-07-17 15:59:06 -0400

    avoid error when app not installed

--- lib/Service/RelatedService.php
+++ lib/Service/RelatedService.php
@@ -153,6 +153,10 @@ class RelatedService {
 	): array {
 		$this->logger->debug('retrieving related to item ' . $providerId . '.' . $itemId);
 
+		if ($providerId && !$this->appManager->isInstalled($providerId)) {
+			return [];
+		}
+
 		try {
 			// we generate a related resource for current item, including a full
 			// list of recipients and virtual group
@@ -602,7 +606,7 @@ class RelatedService {
 			}
 		}
 
-		throw new RelatedResourceProviderNotFound();
+		throw new RelatedResourceProviderNotFound("Failed to find provider {$relatedProviderId}");
 	}
 
 	/**
